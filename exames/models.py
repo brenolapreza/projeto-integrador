1106ddb9013ce9d082ccd0f03d404c258716bc96
brenolapreza <brenolapreza@gmail.com>
2020-05-20 00:50:34 -0300

    Encapsulamento Realizado no models de Exame

--- exames/models.py
+++ exames/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from pacientes.models import Paciente
 from consultas.models import Consulta
 #tipo de exame
-
 URINA = 1
 SANGUE = 2
 VISTA = 3
@@ -12,11 +11,90 @@ TIPO_EXAME = [
     (2, 'Sangue'),
     (3, 'Vista'),
 ]
+#Urgencia do Exame
+LEVE = 1
+GRAVE = 2
+GRAVISSIMO = 3
+
+GRAU_EXAME = [
+    (1, 'Leve'),
+    (2, 'Grave'),
+    (3, 'Gravissimo'),
+]
+#Diagnostico
+SAUDAVEL = 1
+MEDIO = 2
+DOENTE = 3
+
+DIAGNOSTICO_EXAME = [
+    (1, 'Saudavel'),
+    (2, 'Medio'),
+    (1, 'Doente'),
+]
 
 class Exame(models.Model):
     exame_paciente = models.ForeignKey(Paciente, verbose_name="nome do paciente", on_delete=models.CASCADE, null=True, blank=True)
     _tipo = models.PositiveIntegerField("tipo de exame", choices=TIPO_EXAME, default=SANGUE)
-    data_entrega = models.DateTimeField("data de entrega", null=True, blank=True)
+    _grau = models.PositiveIntegerField("Grau do Exame", choices=GRAU_EXAME, default=LEVE)
+    _diagnostico = models.PositiveIntegerField("Diagnostico do Exame", choices=DIAGNOSTICO_EXAME, default=SAUDAVEL)
 
     def __str__(self):
         return self.exame_paciente.nome
+
+    #tipo de exame
+    @property
+    def tipo(self):
+        for code, label in TIPO_EXAME:
+            if self._tipo == code:
+                break
+        return label
+
+    @tipo.setter
+    def set_tipo(self, valor):
+        if valor == 'Urina':
+            self._tipo = URINA
+        elif valor == 'Sangue':
+            self._tipo = SANGUE
+        elif valor == 'Vista':
+            self._tipo = VISTA
+        else:
+            raise ValueError('Escola entre: Urina, Sangue ou Vista')
+    #Urgencia do exame
+    @property
+    def grau(self):
+        for code, label in GRAU_EXAME:
+            if self._tipo == code:
+                break
+        return label
+
+    @grau.setter
+    def grau(self, valor):
+        if valor == 'Leve':
+            self._grau = LEVE
+        elif valor == 'Grave':
+            self._grau = GRAVE
+        elif valor == 'Gravissimo':
+            self._grau = GRAVISSIMO
+        else:
+            raise ValueError('Escola entre: Leve, Grave ou Gravissimo')
+        
+    @property
+    def diagnostico(self):
+        for code, label in TIPO_EXAME:
+            if self._tipo == code:
+                break
+        return label
+
+    @diagnostico.setter
+    def diagnostico(self, valor):
+        if valor == 'Saudavel':
+            self._diagnostico = SAUDAVEL
+        elif valor == 'Medio':
+            self._diagnostico = MEDIO
+        elif valor == 'Doente':
+            self._diagnostico = DOENTE
+        else:
+            raise ValueError('Escola entre: Saudavel, Medio ou Doente')
+
+    
+  
--- pacientes/models.py
+++ pacientes/models.py
@@ -4,14 +4,12 @@ from django.db import models
 class Paciente(models.Model):
     nome = models.CharField("nome", max_length=50, null=True, blank=True)
     cpf = models.CharField("cpf", unique=True, db_index=True, max_length=15, null=True)
-    data_nascimento = models.DateField("Data de nascimento", null=True, blank=True)
     telefone = models.CharField("telefone", max_length=50, null=True, blank=True)
     endereco = models.CharField("endereço", max_length=60, null=True, blank=True)
-    nascionalidade = models.CharField("nascionalidade", max_length=60, null=True, blank=True)
+    nacionalidade = models.CharField("nascionalidade", max_length=60, null=True, blank=True)
     diagnostico = models.TextField("Diagnóstico", max_length=500, null=True, blank=True)
     criado_em = models.DateTimeField(auto_now_add=True, editable=False, null=True)
     editado_em = models.DateTimeField("Última visualização", auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.nome
-
